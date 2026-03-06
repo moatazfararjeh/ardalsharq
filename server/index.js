640067673b92e67c76408dd138b809fdeec1d8b7
Moataz Mohamed AlFararjeh <mmalfararjeh@netways.com>
2026-03-06 19:35:28 +0300

    Fix Express route pattern for SPA catch-all

--- server/index.js
+++ server/index.js
@@ -185,7 +185,7 @@ app.get('/api/products/:id/images', async (req, res) => {
 
 // Serve index.html for all non-API routes (SPA support)
 if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
